44911d9c355b2756a8ac05f44479bd6d7b400f87
Ryu <0x8664>
2022-10-19 14:19:55 +0300

    update

--- backend2/routes/users/createUser.dart
+++ backend2/routes/users/createUser.dart
@@ -4,8 +4,7 @@ import 'package:dart_frog/dart_frog.dart';
 import '../../controllers/users.dart';
 import '../../models/user.dart';
 
-Future<Response> onRequest(RequestContext context)async {
-
-  await UserOps.createUser({'firstName': 'test1','secondName': 'test2'});
-  return Response(body:'done');
+Future<Response> onRequest(RequestContext context) async {
+  await UserOps.createUser({'firstName': 'test1', 'secondName': 'test2'});
+  return Response.json(body: {'name':'ishikawa'});
 }
