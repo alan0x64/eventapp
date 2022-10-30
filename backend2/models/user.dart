4dc7bfff0d4bd4c23f6affb4380f3f5505b13460
Ryu <0x8664>
2022-10-30 06:08:24 +0300

    update

--- backend2/models/user.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ffi';
-
-class User {
-  User(this.info);
-  Map<String,dynamic> info ;
-  // int userID;
-  // String? firstName;
-  // String? secondName;
-}
--- backend2/routes/index.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-
-
-Future<Response> onRequest(RequestContext context) async {
- 
-  // await userCollection.insert({'name': 'jack'});
-
-  return Response(body: 'test');
-}
