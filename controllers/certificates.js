bb5131cf22071c519ca48968ea255538db2e6810
Matt0x64 <darkspace470@gmail.com>
2022-12-20 03:56:07 +0300

    Updated Cert Controllers

---
+++ controllers/certificates.js
@@ -0,0 +1,27 @@
+const express = require("express")()
+const user = require("../models/certificate")
+const RESPONSE = require("../utils/express_api_res")
+require('../utils/delete_from_arr')
+
+
+
+module.exports.createCert = async (req, res) => {     
+    res.send(RESPONSE(res.statusMessage, res.statusCode, "Cert Created"))
+}
+  
+module.exports.deleteCert = async (req, res) => {
+    res.send(RESPONSE(res.statusMessage, res.statusCode, "Cert Deleted"))
+}
+
+module.exports.updateCert = async (req, res) => {  
+    res.send(RESPONSE(res.statusMessage, res.statusCode, "Cert Updated"))
+}
+
+module.exports.getCert = async (req, res) => {
+    res.send(await Cert.findOne({ '_id': req.params.id }))
+}
+
+module.exports.getCerts = async (req, res) => {
+    res.send(await Cert.findOne({ '_id': req.logedinCert.id }))
+}
+
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const express = require("express")()
-const crypto = require("crypto")
+// const crypto = require("crypto")
 const user = require("../models/user")
 const token_collection = require("../models/token")
 const RESPONSE = require("../utils/express_api_res")
